a291ff44022d174b5ac3eecfd8a3c58a4f7b0b53
Tim Lu <tim.lu1488@gmail.com>
2025-01-21 01:47:41 +1100

    removed keys

--- firebase-config.js
+++ firebase-config.js
@@ -4,16 +4,6 @@ import { getDatabase } from "firebase/database"
 // import { getDatabase, ref, set } from "firebase/database";
 
 // Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCw9FGqizpKOKF5ITKZZ9KW0_mQmk16YGw",
-    authDomain: "fuel-tracker-nsw.firebaseapp.com",
-    databaseURL: "https://fuel-tracker-nsw-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "fuel-tracker-nsw",
-    storageBucket: "fuel-tracker-nsw.firebasestorage.app",
-    messagingSenderId: "405030208707",
-    appId: "1:405030208707:web:8b868dcbdcad91813a8e76",
-    measurementId: "G-F0ED3JQZY1"
-  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
